2b9640c47c5bbf4e16ed85002818fe118272373f
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2025-04-25 16:28:16 +0800

    Update commandLine.ts

--- src/functions/commandLine.ts
+++ src/functions/commandLine.ts
@@ -20,13 +20,14 @@ Git repo folder: ${fss.getVcsRoot() ?? '<none>'}
 `;
 		const response = await llms().medium.generateText([
 			system(
-				'You are to analyze the provided shell command to determine if it is safe to run, i.e. will not cause data loss or other unintended consequences to the host system.',
+				'You are to analyze the provided shell command to determine if it is safe to run, i.e. will not cause configuration changes, data loss or other unintended consequences to the host system or remote systems. Reading files/config and modifying files under version control is acceptable.',
 			),
 			user(
-				`The command which is being requested to execute is:\n${command}\n\n\n Think through the dangers of running this command and response with only a single word, either SAFE or DANGEROUS`,
+				`The command which is being requested to execute is:\n${command}\n\n\n Think through the dangers of running this command and response with only a single word, either SAFE, UNSURE or DANGEROUS`,
 			),
 		]);
-		if (response !== 'SAFE') await humanInTheLoop(`Executing the command "${command}" could be dangerous. Please review the command carefully`);
+		// if (response !== 'SAFE')
+		await humanInTheLoop(`Requesting to execute the shell command: ${command}\nCWD: ${getFileSystem().getWorkingDirectory()}\nSafety analysis: ${response}`);
 
 		const result = await execCommand(command);
 		if (result.exitCode !== 0) throw new Error(`Error executing command ${command}. Return code ${result.exitCode}. Err: ${result.stderr}`);
