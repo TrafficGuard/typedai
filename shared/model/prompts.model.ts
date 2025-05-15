3291ea6437cffdd2b0a3dc1d600694d037e35023
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2025-05-15 23:17:08 +0800

    feat: add Prompt model definition

---
+++ shared/model/prompts.model.ts
@@ -0,0 +1,24 @@
+import {GenerateOptions, LlmMessage} from "#shared/model/llm.model";
+
+/**
+ * Prompts make up the users Prompt library, to test, evaluate and refine the LLM prompts.
+ */
+export interface Prompt {
+    id: string;
+    /** The owning user */
+    userId: string;
+    /** The prompt this one was branched from (if any) */
+    parentId?: string;
+    /** If the user has versioning selected in the UI, then saving should save a new version of this prompt with the revisionId incremented, otherwise it should overwrite the current one.  */
+    revisionId: number;
+    /** The user provided name for the prompt */
+    name: string;
+    /** If a prompt was from an agent execution, then this is the id assigned in the code */
+    appId?: string;
+    /** Tags for categorising and searching */
+    tags: string[];
+    /** The LLM prompt messages */
+    messages: LlmMessage[];
+    /** The options (temperature, topP etc) to generate the response with */
+    options: GenerateOptions
+}
