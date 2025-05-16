6d0dc7d26366730e0ea119a6dced4adcb5a83fe4
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2025-05-16 19:31:03 +0800

    style: Adjust prompt form summary animation timing

--- frontend/src/app/modules/prompts/form/prompt-form.component.ts
+++ frontend/src/app/modules/prompts/form/prompt-form.component.ts
@@ -63,10 +63,10 @@ import { takeUntil, finalize, tap, filter } from 'rxjs/operators';
     trigger('summaryFade', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ opacity: 1 })),
+        animate('250ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ opacity: 1 })),
       ]),
       transition(':leave', [
-        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ opacity: 0 })),
+        animate('250ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ opacity: 0 })),
       ]),
     ]),
   ],
