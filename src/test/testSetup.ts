6e286a7eb3acd8343b7711fbf5af4303d2462f32
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2025-06-23 13:04:13 +0800

    Revert "Update testSetup.ts"
    This reverts commit a3b36631f90a338cd9acab76a411a8d14fe139a2.

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,11 +1,5 @@
 import '#fastify/trace-init/trace-init'; // leave an empty line next so this doesn't get sorted from the first line
 
-import { copyFileSync, existsSync } from 'node:fs';
 import { agentContext } from '#agent/agentContextLocalStorage';
 
 agentContext();
-
-if (!existsSync('variables/test.env')) {
-	console.log('Copying variables/local.env.example to variables/test.env');
-	copyFileSync('variables/local.env.example', 'variables/test.env');
-}
